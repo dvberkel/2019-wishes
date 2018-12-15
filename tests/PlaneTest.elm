935b4d31ad5efa9e4818dd6d6956f458291e15dc
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-12-15 14:30:01 +0100

    Create multiple tests

--- tests/PlaneTest.elm
+++ tests/PlaneTest.elm
@@ -3,7 +3,7 @@ module PlaneTest exposing (suite)
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Plane exposing (..)
-import Plane.Compass exposing (..)
+import Plane.Compass as Compass exposing (..)
 import Plane.Position exposing (..)
 import Test exposing (..)
 
@@ -13,22 +13,35 @@ suite =
     describe "Plane"
         [ describe "move"
             ([ ( North, ( 0, 1 ) )
+             , ( East, ( 1, 0 ) )
+             , ( South, ( 0, -1 ) )
+             , ( West, ( -1, 0 ) )
              ]
                 |> List.map
                     (\( direction, ( x, y ) ) ->
-                        test "moving the plane changes position" <|
+                        let
+                            name =
+                                "moving the plane "
+                                    ++ Compass.toString direction
+                                    ++ " changes location to ("
+                                    ++ String.fromInt x
+                                    ++ ", "
+                                    ++ String.fromInt y
+                                    ++ ")"
+                        in
+                        test name <|
                             \_ ->
                                 let
                                     actual =
                                         plane
                                             |> at (position 0 0)
-                                            |> heading North
+                                            |> heading direction
                                             |> move
 
                                     expected =
                                         plane
-                                            |> at (position 0 1)
-                                            |> heading North
+                                            |> at (position x y)
+                                            |> heading direction
                                             |> withTail [ position 0 0 ]
                                 in
                                 actual
