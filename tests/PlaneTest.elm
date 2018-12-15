8e9620cd106127b2ecc3cf3fe3c507ae8db4077f
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-12-15 14:47:04 +0100

    Test with different starting positions

--- tests/PlaneTest.elm
+++ tests/PlaneTest.elm
@@ -16,6 +16,10 @@ suite =
              , { from = ( 0, 0 ), direction = East, to = ( 1, 0 ) }
              , { from = ( 0, 0 ), direction = South, to = ( 0, -1 ) }
              , { from = ( 0, 0 ), direction = West, to = ( -1, 0 ) }
+             , { from = ( 1, 2 ), direction = North, to = ( 1, 3 ) }
+             , { from = ( 2, 3 ), direction = East, to = ( 3, 3 ) }
+             , { from = ( 3, 5 ), direction = South, to = ( 3, 4 ) }
+             , { from = ( 5, 8 ), direction = West, to = ( 4, 8 ) }
              ]
                 |> List.map
                     (\{ from, direction, to } ->
