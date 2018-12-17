caa62bd5da2d76ceeef0b80d3979dbe200925884
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-12-17 19:14:25 +0100

    Create a Browser.element

--- src/Wish.elm
+++ src/Wish.elm
@@ -1,11 +1,33 @@
 module Wish exposing (main)
 
-import Plane exposing (plane, at, heading, withTail)
-import Plane.Position exposing (position)
+import Browser
+import Html exposing (Html)
+import Plane exposing (Plane, at, heading, plane, withTail)
 import Plane.Compass exposing (Compass(..))
+import Plane.Position exposing (position)
 import Rendering.Html as Rendering
 
+
+main : Program () Model msg
 main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- Model
+
+
+type alias Model =
+    { plane : Plane }
+
+
+init : flag -> ( Model, Cmd msg)
+init _ =
     let
         aPlane =
             plane
@@ -13,6 +35,25 @@ main =
                 |> heading North
                 |> withTail 4 [ position 1 1, position 1 0, position 0 0 ]
     in
-        aPlane
-            |> Plane.render
-            |> Rendering.toHtml
+    ( { plane = aPlane }, Cmd.none )
+
+
+-- View
+
+view : Model -> Html msg
+view model =
+    model.plane
+        |> Plane.render
+        |> Rendering.toHtml
+
+-- Update
+
+update: msg -> Model -> (Model, Cmd msg)
+update _ model =
+    (model, Cmd.none)
+
+-- Subscriptions
+
+subscriptions: Model -> Sub msg
+subscriptions _ =
+    Sub.none
